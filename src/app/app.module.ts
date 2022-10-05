51a0dc7238d90f424ef76d49c28fd8b555820e6a
Norinot <naruto010190@gmail.com>
2022-10-05 14:37:32 +0200

    login component and login service implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes,RouterModule } from '@angular/router';
+
+  const routes: Routes = [
+    { path: 'div-main', component: MainComponent },
+    { path: 'login', component: LoginPageComponent },
+  ];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { interval } from 'rxjs';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent implements OnInit, OnDestroy {
+  login_form: FormGroup;
+  invalidLoginMessage: boolean = false;
+
+  constructor(fb: FormBuilder, public loginService: LoginService) {
+    this.login_form = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    console.log(this.loginService.loggedIn);
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    this.loginService.login(
+      this.login_form.value.email,
+      this.login_form.value.password
+    );
+    if (this.loginService.loggedIn === false) {
+      this.invalidLoginMessage = true;
+      setTimeout(() => {
+        this.invalidLoginMessage = false;
+      }, 3000);
+    }
+  }
+
+  ngOnDestroy(): void {}
+}
