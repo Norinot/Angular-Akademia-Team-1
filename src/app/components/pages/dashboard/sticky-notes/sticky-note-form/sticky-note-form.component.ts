8db8e4abe081cfe2b6a8050ecc570f46f577a95a
zsofip <zsofia.pichler@gmail.com>
2022-10-06 15:03:34 +0200

    sticky note model and form changed

--- src/app/components/pages/dashboard/sticky-notes/sticky-note-form/sticky-note-form.component.ts
+++ src/app/components/pages/dashboard/sticky-notes/sticky-note-form/sticky-note-form.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { StickyNote } from './../sticky-note.model';
 import { StickyNoteService } from './../../../../../services/sticky-note.service';
 import { Component, OnInit } from '@angular/core';
@@ -12,22 +13,28 @@ export class StickyNoteFormComponent implements OnInit {
 
   notesForm: FormGroup;
 
-  constructor(fb: FormBuilder, private notesService: StickyNoteService) {
+  constructor(fb: FormBuilder, private notesService: StickyNoteService, private router: Router, private route: ActivatedRoute) {
     this.notesForm = fb.group({
-      note: [""]
+      title: [""],
+      content: [""]
     });
   }
 
   ngOnInit(): void {
   }
 
-  get note(): FormControl {
+  get title(): FormControl {
+    return this.notesForm.get('note') as FormControl;
+  }
+
+  get content(): FormControl {
     return this.notesForm.get('note') as FormControl;
   }
 
   onSubmit() {
     const savedNote: StickyNote = this.notesForm.value as StickyNote;
-    this.notesService.addNote(savedNote) ;
+    this.notesService.addNote(savedNote);
+    // this.router.navigate(['/', 'sticky-notes-container', '/', 'sticky-note']);
   }
 
 }
