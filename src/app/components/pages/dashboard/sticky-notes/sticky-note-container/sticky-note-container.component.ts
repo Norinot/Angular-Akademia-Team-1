4af6185a5bb07463fbdf2be255cf64a1c8c72b52
Norinot <naruto010190@gmail.com>
2022-10-07 13:41:37 +0200

    alignment options for sticky notes implementation

--- src/app/components/pages/dashboard/sticky-notes/sticky-note-container/sticky-note-container.component.ts
+++ src/app/components/pages/dashboard/sticky-notes/sticky-note-container/sticky-note-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StickyNoteService } from 'src/app/services/sticky-note.service';
 
 @Component({
   selector: 'app-sticky-note-container',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StickyNoteContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public stickyService: StickyNoteService) { }
 
   ngOnInit(): void {
   }
--- src/app/services/sticky-note.service.ts
+++ src/app/services/sticky-note.service.ts
@@ -5,8 +5,42 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class StickyNoteService {
-
   notes: Array<any> = [];
+  valami: boolean = false;
+  classes: any[] = [
+    {
+      AlignClass: 'align-content-flex-start',
+      imgUrl: '../../assets/Alignment-icons/align-top.png',
+    },
+    {
+      AlignClass: 'align-content-center',
+      imgUrl: '../../assets/Alignment-icons/align-center.png',
+
+    },
+    {
+      AlignClass: 'align-content-flex-end',
+      imgUrl: '../../assets/Alignment-icons/align-bottom.png',
+
+    },
+    {
+      JustifyClass: 'justify-content-flex-start',
+      imgUrl: '../../assets/Alignment-icons/justify-left.svg',
+
+    },
+    {
+      JustifyClass: 'justify-content-flex-center',
+      imgUrl: '../../assets/Alignment-icons/justify-center.png',
+
+    },
+    {
+      JustifyClass: 'justify-content-flex-end',
+      imgUrl: '../../assets/Alignment-icons/justify-right.png',
+
+    },
+  ];
+
+  selectedAlignContent: string = '';
+  selectedJustifyContent: string = '';
 
   constructor() {
     this.notes = [
@@ -38,13 +72,33 @@ export class StickyNoteService {
   }
 
   public getNotebyId(id: number) {
-    const index = this.notes.findIndex(item => item.id == id);
+    const index = this.notes.findIndex((item) => item.id == id);
     return this.notes[index];
   }
 
   public deleteNote(note: StickyNote) {
-    const index = this.notes.findIndex(item => item.id == note.id);
+    const index = this.notes.findIndex((item) => item.id == note.id);
     this.notes.splice(index, 1);
+  }
 
+  onAddStyling(item: any) {
+    if (item.AlignClass) {
+      if (item.AlignClass === this.selectedAlignContent) {
+        // Ha megyegyezik egy kiválasztott classt
+        this.selectedAlignContent = '';
+      } else {
+        this.selectedAlignContent = item.AlignClass;
+      }
+    }
+    console.log(item.AlignClass);
+
+    if (item.JustifyClass) {
+      if (item.JustifyClass === this.selectedJustifyContent) {
+        // Ha megyegyezik egy kiválasztott classt
+        this.selectedJustifyContent = '';
+      } else {
+        this.selectedJustifyContent = item.JustifyClass;
+      }
+    }
   }
 }
